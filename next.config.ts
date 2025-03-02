35917668583db7212fc1b2bfcbf70b72a4635002
TryCaze <trycaze@gmail.com>
2025-03-02 18:41:41 +0100

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "hope"; // Replace with your actual repo name
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
-  images: {
-    unoptimized: true, // Disable Next.js image optimization for GitHub Pages
-  },
+  /* config options here */
 };
 
 export default nextConfig;
