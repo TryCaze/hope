2c51bbd638ebd20bf3798d4c4ba8e80ffe27a741
TryCaze <trycaze@gmail.com>
2025-03-02 18:03:29 +0100

    Config file

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/hope' : '',
+  assetPrefix: isProd ? '/hope/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+
 export default nextConfig;
